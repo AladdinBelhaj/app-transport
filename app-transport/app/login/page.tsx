46e5c430d946acae4171760b3fb7d5e0597faf86
aladdin <alabelhaj2002@gmail.com>
2024-02-05 15:31:00 +0100

    Add and edit login form

--- app-transport/app/login/page.tsx
+++ app-transport/app/login/page.tsx
@@ -21,49 +21,39 @@ const login = () => {
               src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
               alt="Your Company"
             />
-            <h2 className="mt-1 text-center text-xs font-bold leading-3 tracking-tight text-gray-900">
-              Sign in to your account
-            </h2>
+            <h2 className={styles.h2}>Sign in to your account</h2>
             <form className="space-y-2" action="#" method="POST">
               <div>
-                <label
-                  htmlFor="email"
-                  className={
-                    "block text-xs font-medium leading-6 text-gray-900"
-                  }
-                >
+                <label htmlFor="email" className={styles.label}>
                   Email address
                 </label>
-                <div className="mt-2">
+                <div className="mt-1">
                   <input
                     id="email"
                     name="email"
                     type="email"
                     autoComplete="email"
                     required
-                    className="block w-full rounded-md border-0 py-0.5 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
+                    className="block w-full rounded-md border-0 py-0 text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
                   />
                 </div>
               </div>
 
               <div>
                 <div className="flex items-center justify-between">
-                  <label
-                    htmlFor="password"
-                    className="block text-xs font-medium leading-6 text-gray-900"
-                  >
+                  <label htmlFor="password" className={styles.label}>
                     Password
                   </label>
                   <div className="text-sx">
                     <a
                       href="#"
-                      className="font-semibold text-sm text-indigo-600 hover:text-indigo-500"
+                      className="font-semibold text-xs text-indigo-600 hover:text-indigo-500"
                     >
                       Forgot password?
                     </a>
                   </div>
                 </div>
-                <div className="mt-2">
+                <div className="mt-1">
                   <input
                     id="password"
                     name="password"
@@ -78,7 +68,7 @@ const login = () => {
               <div>
                 <button
                   type="submit"
-                  className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-xs font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
+                  className="flex w-full justify-center rounded-md bg-indigo-600 px-1 py-0.5 text-xs font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
                 >
                   Sign in
                 </button>
