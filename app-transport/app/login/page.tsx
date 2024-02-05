8fff90d5816beda26c5312b6e8ffe21c728c08ac
aladdin <alabelhaj2002@gmail.com>
2024-02-05 12:39:23 +0100

    Set up login background

---
+++ app-transport/app/login/page.tsx
@@ -0,0 +1,31 @@
+import React from "react";
+import styles from "./Login.module.css";
+import Image from "next/image";
+const login = () => {
+  return (
+    <main className={styles.main}>
+      <div className={styles.left}>
+        <Image
+          className={styles.img}
+          src="/assets/bg-login.png"
+          alt=""
+          width={900}
+          height={400}
+        />
+      </div>
+
+      <div className={styles.right}>
+        <div className="card w-95 h-95 bg-base-100 shadow-xl">
+          <div className="card-body">
+            {/* <h2 className="card-title">Card title!</h2> */}
+            <div className="card-actions justify-end">
+              <button className="btn btn-primary">Buy Now</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </main>
+  );
+};
+
+export default login;
--- app-transport/app/users/page.tsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const usersPage = () => {
-  return <div>UsersPage</div>;
-};
-
-export default usersPage;
