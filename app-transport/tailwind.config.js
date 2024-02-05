5ef25ec0fea2022502a429f960357ee98d73a906
aladdin <alabelhaj2002@gmail.com>
2024-02-05 10:11:55 +0100

    Fix tailwind issue

--- app-transport/tailwind.config.js
+++ app-transport/tailwind.config.js
@@ -1,9 +1,7 @@
-const { default: daisyui } = require('daisyui');
-
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    "./src/**/*.{js,ts,tsx,jsx}"
+    "./src/**/*.{js,ts,jsx,tsx}"
   ],
   theme: {
     extend: {},
