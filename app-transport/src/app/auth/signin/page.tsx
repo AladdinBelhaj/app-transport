e484fd43793b2cf11ac2f102098f853200e87a44
aladdin <alabelhaj2002@gmail.com>
2024-02-10 15:18:53 +0100

    Add icon to email validation to sign up

--- app-transport/src/app/auth/signin/page.tsx
+++ app-transport/src/app/auth/signin/page.tsx
@@ -5,7 +5,8 @@ import Link from "next/link";
 import style from "./signin.module.css";
 import { useState } from "react";
 import axios from "axios";
-
+import EmailValidation from "./EmailValidation";
+import PasswordValidation from "./PasswordValidation";
 const Login = () => {
   const [post, setPost] = useState({
     email: "",
@@ -89,76 +90,8 @@ const Login = () => {
                 </h2>
 
                 <form>
-                  <div className="mb-4">
-                    <label className="mb-2.5 block font-medium text-black dark:text-white">
-                      Email
-                    </label>
-                    <div className="relative">
-                      <input
-                        type="email"
-                        onChange={(e) => handleInput("email", e.target.value)}
-                        placeholder="Enter your email"
-                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      />
-
-                      <span className="absolute right-4 top-4">
-                        <svg
-                          className="fill-current"
-                          width="22"
-                          height="22"
-                          viewBox="0 0 22 22"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g opacity="0.5">
-                            <path
-                              d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
-                              fill=""
-                            />
-                          </g>
-                        </svg>
-                      </span>
-                    </div>
-                  </div>
-
-                  <div className="mb-6">
-                    <label className="mb-2.5 block font-medium text-black dark:text-white">
-                      Password
-                    </label>
-                    <div className="relative">
-                      <input
-                        type="password"
-                        name="password"
-                        onChange={(e) =>
-                          handleInput("password", e.target.value)
-                        }
-                        placeholder="Enter your password"
-                        className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
-                      />
-
-                      <span className="absolute right-4 top-4">
-                        <svg
-                          className="fill-current"
-                          width="22"
-                          height="22"
-                          viewBox="0 0 22 22"
-                          fill="none"
-                          xmlns="http://www.w3.org/2000/svg"
-                        >
-                          <g opacity="0.5">
-                            <path
-                              d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
-                              fill=""
-                            />
-                            <path
-                              d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
-                              fill=""
-                            />
-                          </g>
-                        </svg>
-                      </span>
-                    </div>
-                  </div>
+                  <EmailValidation></EmailValidation>
+                  <PasswordValidation></PasswordValidation>
 
                   <div className="mb-5">
                     <input
@@ -226,116 +159,81 @@ const Login = () => {
 
 export default Login;
 
-// <main className={styles.main}>
-// <div className={styles.left}>
-//   <Image
-//     className={styles.img}
-//     src="/assets/bg-login.png"
-//     alt=""
-//     width={900}
-//     height={400}
-//   />
+// email
 
-//   <div className="card bg-base-100 h-3/5 w-1/4 shadow-xl">
-//     <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-12 lg:px-8">
-//       <div className="card-body">
-//         <img
-//           className="mx-auto h-3 w-auto"
-//           src="https://tailwindui.com/img/logos/mark.svg?color=indigo&shade=600"
-//           alt="Your Company"
-//         />
+{
+  /* <div className="mb-4">
+<label className="mb-2.5 block font-medium text-black dark:text-white">
+  Email
+</label>
+<div className="relative">
+  <input
+    type="email"
+    onChange={(e) => handleInput("email", e.target.value)}
+    placeholder="Enter your email"
+    className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+  />
 
-//         <svg
-//           xmlns="http://www.w3.org/2000/svg"
-//           fill="none"
-//           viewBox="0 0 24 24"
-//           strokeWidth={1.5}
-//           stroke="#4f46e5"
-//           className="mx-auto h-8 w-auto"
-//         >
-//           <path
-//             strokeLinecap="round"
-//             strokeLinejoin="round"
-//             d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-//           />
-//         </svg>
+  <span className="absolute right-4 top-4">
+    <svg
+      className="fill-current"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.5">
+        <path
+          d="M19.2516 3.30005H2.75156C1.58281 3.30005 0.585938 4.26255 0.585938 5.46567V16.6032C0.585938 17.7719 1.54844 18.7688 2.75156 18.7688H19.2516C20.4203 18.7688 21.4172 17.8063 21.4172 16.6032V5.4313C21.4172 4.26255 20.4203 3.30005 19.2516 3.30005ZM19.2516 4.84692C19.2859 4.84692 19.3203 4.84692 19.3547 4.84692L11.0016 10.2094L2.64844 4.84692C2.68281 4.84692 2.71719 4.84692 2.75156 4.84692H19.2516ZM19.2516 17.1532H2.75156C2.40781 17.1532 2.13281 16.8782 2.13281 16.5344V6.35942L10.1766 11.5157C10.4172 11.6875 10.6922 11.7563 10.9672 11.7563C11.2422 11.7563 11.5172 11.6875 11.7578 11.5157L19.8016 6.35942V16.5688C19.8703 16.9125 19.5953 17.1532 19.2516 17.1532Z"
+          fill=""
+        />
+      </g>
+    </svg>
+  </span>
+</div>
+</div> */
+}
 
-//         <h2 className="text-gray-900 mt-1 text-center text-sm font-bold leading-3 tracking-tight">
-//           Sign in to your account
-//         </h2>
-//       </div>
-//       <div className="mt-10 sm:mx-auto sm:w-full sm:max-w-sm">
-//         <form className="space-y-6" action="#" method="POST">
-//           <div>
-//             <label
-//               htmlFor="email"
-//               className="text-gray-900 block text-sm font-medium leading-6"
-//             >
-//               Email address
-//             </label>
-//             <div className="mt-2">
-//               <input
-//                 id="email"
-//                 name="email"
-//                 type="email"
-//                 autoComplete="email"
-//                 required
-//                 className="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-//               />
-//             </div>
-//           </div>
+//password
 
-//           <div>
-//             <div className="flex items-center justify-between">
-//               <label
-//                 htmlFor="password"
-//                 className="text-gray-900 block text-sm font-medium leading-6"
-//               >
-//                 Password
-//               </label>
-//               <div className="text-sm">
-//                 <a
-//                   href="#"
-//                   className="font-semibold text-indigo-600 hover:text-indigo-500"
-//                 >
-//                   Forgot password?
-//                 </a>
-//               </div>
-//             </div>
-//             <div className="mt-2">
-//               <input
-//                 id="password"
-//                 name="password"
-//                 type="password"
-//                 autoComplete="current-password"
-//                 required
-//                 className="text-gray-900 ring-gray-300 placeholder:text-gray-400 block w-full rounded-md border-0 py-1.5 shadow-sm ring-1 ring-inset focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-sm sm:leading-6"
-//               />
-//             </div>
-//           </div>
+{
+  /* <div className="mb-6">
+<label className="mb-2.5 block font-medium text-black dark:text-white">
+  Password
+</label>
+<div className="relative">
+  <input
+    type="password"
+    name="password"
+    onChange={(e) =>
+      handleInput("password", e.target.value)
+    }
+    placeholder="Enter your password"
+    className="w-full rounded-lg border border-stroke bg-transparent py-4 pl-6 pr-10 text-black outline-none focus:border-primary focus-visible:shadow-none dark:border-form-strokedark dark:bg-form-input dark:text-white dark:focus:border-primary"
+  />
 
-//           <div>
-//             <button
-//               type="submit"
-//               className="flex w-full justify-center rounded-md bg-indigo-600 px-3 py-1.5 text-sm font-semibold leading-6 text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600"
-//             >
-//               Sign in
-//             </button>
-//           </div>
-//         </form>
-
-//         <p className="text-gray-500 mt-10 text-center text-sm">
-//           Not a member?{" "}
-//           <a
-//             href="#"
-//             className="font-semibold leading-6 text-indigo-600 hover:text-indigo-500"
-//           >
-//             Create an account!
-//           </a>
-//           n
-//         </p>
-//       </div>
-//     </div>
-//   </div>
-// </div>
-// </main>
+  <span className="absolute right-4 top-4">
+    <svg
+      className="fill-current"
+      width="22"
+      height="22"
+      viewBox="0 0 22 22"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
+    >
+      <g opacity="0.5">
+        <path
+          d="M16.1547 6.80626V5.91251C16.1547 3.16251 14.0922 0.825009 11.4797 0.618759C10.0359 0.481259 8.59219 0.996884 7.52656 1.95938C6.46094 2.92188 5.84219 4.29688 5.84219 5.70626V6.80626C3.84844 7.18438 2.33594 8.93751 2.33594 11.0688V17.2906C2.33594 19.5594 4.19219 21.3813 6.42656 21.3813H15.5016C17.7703 21.3813 19.6266 19.525 19.6266 17.2563V11C19.6609 8.93751 18.1484 7.21876 16.1547 6.80626ZM8.55781 3.09376C9.31406 2.40626 10.3109 2.06251 11.3422 2.16563C13.1641 2.33751 14.6078 3.98751 14.6078 5.91251V6.70313H7.38906V5.67188C7.38906 4.70938 7.80156 3.78126 8.55781 3.09376ZM18.1141 17.2906C18.1141 18.7 16.9453 19.8688 15.5359 19.8688H6.46094C5.05156 19.8688 3.91719 18.7344 3.91719 17.325V11.0688C3.91719 9.52189 5.15469 8.28438 6.70156 8.28438H15.2953C16.8422 8.28438 18.1141 9.52188 18.1141 11V17.2906Z"
+          fill=""
+        />
+        <path
+          d="M10.9977 11.8594C10.5852 11.8594 10.207 12.2031 10.207 12.65V16.2594C10.207 16.6719 10.5508 17.05 10.9977 17.05C11.4102 17.05 11.7883 16.7063 11.7883 16.2594V12.6156C11.7883 12.2031 11.4102 11.8594 10.9977 11.8594Z"
+          fill=""
+        />
+      </g>
+    </svg>
+  </span>
+</div>
+</div> */
+}
