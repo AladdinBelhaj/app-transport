ab4347980bbf1aea4edcd46ed8accb17407947d5
aladdin <alabelhaj2002@gmail.com>
2024-02-16 17:02:46 +0100

    Add calendar and modal that reacts to cell grids when clicked

--- app-transport/src/app/calendar/page.tsx
+++ app-transport/src/app/calendar/page.tsx
@@ -1,4 +1,4 @@
-import Calendar from "@/components/Calender";
+import Calendar from "@/components/Calender/ORIGINAL";
 import { Metadata } from "next";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
 
