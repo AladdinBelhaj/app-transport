057c1a4322b5eecaec0cb1bd69d5685277d8ba6f
aladdin <alabelhaj2002@gmail.com>
2024-03-16 14:34:14 +0100

    display objects photos

--- app-transport/src/app/view-offers/page.tsx
+++ app-transport/src/app/view-offers/page.tsx
@@ -152,247 +152,17 @@ const ViewOffers = () => {
     };
   }, [isModalOpen]);
 
-  const [isDeleteModalOpen, setIsDeleteModalOpen] = useState(false);
-  const [isAcceptModalOpen, setIsAcceptModalOpen] = useState(false);
-  const [offerToDeleteId, setOfferToDeleteId] = useState<number | null>(null);
-  const [offerToAcceptId, setOfferToAcceptId] = useState<number | null>(null);
-
-  const openDeleteModal = (offerId: number) => {
-    setIsDeleteModalOpen(true);
-    setOfferToDeleteId(offerId); // Set the offer ID to be deleted
-  };
-
-  const closeDeleteModal = () => {
-    setIsDeleteModalOpen(false);
-  };
-
-  const openAcceptModal = (offerId: number) => {
-    setIsAcceptModalOpen(true);
-    setOfferToAcceptId(offerId); // Set the offer ID to be deleted
-  };
-
-  const closeAcceptModal = () => {
-    setIsAcceptModalOpen(false);
-  };
-
-  const handleDelete = () => {
-    if (offerToDeleteId !== null) {
-      // Remove the rejected offer from the UI
-      setOfferData((prevOfferData) =>
-        prevOfferData.filter((offer: any) => offer.id !== offerToDeleteId),
-      );
-      setIsDeleteModalOpen(false); // Close the delete modal
-      setOfferToDeleteId(null); // Reset the offer ID to null
-
-      axios
-        .put(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/offers/${offerToAcceptId}`,
-          { status: "rejected" },
-        )
-        .then((response) => {
-          console.log("Offer rejected successfully");
-        })
-        .catch((error) => {
-          console.error("Error rejecting offer:", error);
-        });
-    }
-  };
-
-  const handleAccept = () => {
-    if (offerToAcceptId !== null) {
-      // Remove the rejected offer from the UI
-      setOfferData((prevOfferData) =>
-        prevOfferData.filter((offer: any) => offer.id !== offerToAcceptId),
-      );
-      setIsAcceptModalOpen(false); // Close the delete modal
-      setOfferToAcceptId(null); // Reset the offer ID to null
-
-      axios
-        .put(
-          `${process.env.NEXT_PUBLIC_BACKEND_URL}/api/offers/${offerToAcceptId}`,
-          { status: "accepted" },
-        )
-        .then((response) => {
-          console.log("Offer accepted successfully");
-        })
-        .catch((error) => {
-          console.error("Error accepting offer:", error);
-        });
-    }
-  };
-
   return (
     <DefaultLayout>
       <Breadcrumb pageName="Current Offers" />
-      {/* Delete Modal */}
-      {isDeleteModalOpen && (
-        <div
-          id="deleteModal"
-          tabIndex={-1}
-          aria-hidden="true"
-          className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden"
-          style={{ zIndex: 9999, backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-        >
-          <div className="relative w-full max-w-md">
-            {/* Modal content */}
-            <div className="dark:bg-gray-800 relative rounded-lg bg-white p-4 text-center shadow sm:p-5">
-              <button
-                type="button"
-                onClick={closeDeleteModal}
-                className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 absolute right-2.5 top-2.5 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm dark:hover:text-white"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-                <span className="sr-only">Close modal</span>
-              </button>
-              <svg
-                className="text-gray-400 dark:text-gray-500 mx-auto mb-3.5 h-11 w-11"
-                aria-hidden="true"
-                fill="currentColor"
-                viewBox="0 0 20 20"
-                xmlns="http://www.w3.org/2000/svg"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0v-6zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                  clipRule="evenodd"
-                />
-              </svg>
-              <p className="text-gray-500 dark:text-gray-300 mb-4">
-                Are you sure you want to reject this offer?
-              </p>
-              <div className="flex items-center justify-center space-x-4">
-                <button
-                  type="button"
-                  className="text-gray-500 border-gray-200 hover:bg-gray-100 hover:text-gray-900 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-600 rounded-lg border bg-white px-3 py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-4 focus:ring-modal-300 dark:hover:text-white"
-                  onClick={closeDeleteModal} // Close modal on click
-                >
-                  No, cancel
-                </button>
-                <button
-                  type="submit"
-                  className="inline-flex items-center rounded-lg bg-red-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-red-700 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
-                  onClick={() => handleDelete()}
-                >
-                  <svg
-                    aria-hidden="true"
-                    className="-ml-1 mr-1.5 h-5 w-5"
-                    fill="currentColor"
-                    viewBox="0 0 20 20"
-                    xmlns="http://www.w3.org/2000/svg"
-                  >
-                    <path
-                      fillRule="evenodd"
-                      d="M9 2a1 1 0 00-.894.553L7.382 4H4a1 1 0 000 2v10a2 2 0 002 2h8a2 2 0 002-2V6a1 1 0 100-2h-3.382l-.724-1.447A1 1 0 0011 2H9zM7 8a1 1 0 012 0v6a1 1 0 11-2 0v-6zm5-1a1 1 0 00-1 1v6a1 1 0 102 0V8a1 1 0 00-1-1z"
-                      clipRule="evenodd"
-                    />
-                  </svg>
-                  Reject
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {/* End Delete Modal */}
-      {/* Accept Modal */}
-      {isAcceptModalOpen && (
-        <div
-          id="acceptModal"
-          tabIndex={-1}
-          aria-hidden="true"
-          className="fixed inset-0 z-50 flex items-center justify-center overflow-y-auto overflow-x-hidden"
-          style={{ zIndex: 9999, backgroundColor: "rgba(0, 0, 0, 0.5)" }}
-        >
-          <div className="relative w-full max-w-md">
-            {/* Modal content */}
-            <div className="dark:bg-gray-800 relative rounded-lg bg-white p-4 text-center shadow sm:p-5">
-              <button
-                type="button"
-                onClick={closeAcceptModal}
-                className="text-gray-400 hover:bg-gray-200 hover:text-gray-900 dark:hover:bg-gray-600 absolute right-2.5 top-2.5 ml-auto inline-flex items-center rounded-lg bg-transparent p-1.5 text-sm dark:hover:text-white"
-              >
-                <svg
-                  aria-hidden="true"
-                  className="h-5 w-5"
-                  fill="currentColor"
-                  viewBox="0 0 20 20"
-                  xmlns="http://www.w3.org/2000/svg"
-                >
-                  <path
-                    fillRule="evenodd"
-                    d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z"
-                    clipRule="evenodd"
-                  />
-                </svg>
-
-                <span className="sr-only">Close modal</span>
-              </button>
-              <svg
-                className="text-gray-400 dark:text-gray-500 mx-auto mb-3.5 h-11 w-11"
-                aria-hidden="true"
-                xmlns="http://www.w3.org/2000/svg"
-                fill="currentColor"
-                viewBox="0 0 24 24"
-              >
-                <path
-                  fill-rule="evenodd"
-                  d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z"
-                  clip-rule="evenodd"
-                />
-              </svg>
-              <p className="text-gray-500 dark:text-gray-300 mb-4">
-                Are you sure you want to accept this offer?
-              </p>
-              <div className="flex items-center justify-center space-x-4">
-                <button
-                  type="button"
-                  className="text-gray-500 border-gray-200 hover:bg-gray-100 hover:text-gray-900 dark:bg-gray-700 dark:text-gray-300 dark:border-gray-500 dark:hover:bg-gray-600 dark:focus:ring-gray-600 rounded-lg border bg-white px-3 py-2 text-sm font-medium focus:z-10 focus:outline-none focus:ring-4 focus:ring-modal-300 dark:hover:text-white"
-                  onClick={closeAcceptModal} // Close modal on click
-                >
-                  No, cancel
-                </button>
-                <button
-                  type="submit"
-                  className="inline-flex items-center rounded-lg bg-green-600 px-5 py-2.5 text-center text-sm font-medium text-white hover:bg-green-700 focus:outline-none focus:ring-4 focus:ring-red-300 dark:bg-red-500 dark:hover:bg-red-600 dark:focus:ring-red-900"
-                  onClick={() => handleAccept()}
-                >
-                  <svg
-                    className="-ml-1 mr-1.5 h-5 w-5"
-                    aria-hidden="true"
-                    xmlns="http://www.w3.org/2000/svg"
-                    fill="currentColor"
-                    viewBox="0 0 24 24"
-                  >
-                    <path
-                      fill-rule="evenodd"
-                      d="M2 12a10 10 0 1 1 20 0 10 10 0 0 1-20 0Zm13.7-1.3a1 1 0 0 0-1.4-1.4L11 12.6l-1.8-1.8a1 1 0 0 0-1.4 1.4l2.5 2.5c.4.4 1 .4 1.4 0l4-4Z"
-                      clip-rule="evenodd"
-                    />
-                  </svg>
-                  Accept
-                </button>
-              </div>
-            </div>
-          </div>
-        </div>
-      )}
-      {/* End Delete Modal */}
-
       {offerData.map((offer: any, offerIndex: number) => {
         const tripData = tripDataMap[offer.tripId];
         const userData = userDataMap[offer.userId];
+        const pictures = JSON.parse(offer.picture);
+        const pictureIds = JSON.parse(offer.pictureIds);
+
+        console.log("pictures:", pictures);
+        console.log("pictureIds:", pictureIds);
         return (
           <div
             className="dark:border-stroked mb-10 rounded-sm border border-stroke bg-white shadow-default"
@@ -452,17 +222,18 @@ const ViewOffers = () => {
                       className="h-15 w-17.5 cursor-pointer rounded-md"
                       onClick={() =>
                         openModal(
-                          `${process.env.NEXT_PUBLIC_BACKEND_URL}/${offer.picture}`,
+                          `${process.env.NEXT_PUBLIC_BACKEND_URL}/${pictures[index]}`,
                         )
                       }
                     >
                       <img
-                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${offer.picture}`}
+                        src={`${process.env.NEXT_PUBLIC_BACKEND_URL}/${pictures[index]}`}
                         width={60}
                         height={50}
                         alt="Product"
                       />
                     </div>
+
                     <p className="text-sm text-black dark:text-white">
                       {object[`name-${index}`]}
                     </p>
