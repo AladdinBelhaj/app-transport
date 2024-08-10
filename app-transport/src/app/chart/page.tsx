39011c315330b6339f1f3057842346e2128b3989
aladdin <alabelhaj2002@gmail.com>
2024-08-10 14:45:00 +0100

    fix build issue

--- app-transport/src/app/chart/page.tsx
+++ app-transport/src/app/chart/page.tsx
@@ -1,11 +1,34 @@
-"use client";
-import Chart from "@/components/Charts/page";
+// "use client";
+// import Chart from "@/components/Charts/page";
 
+// import DefaultLayout from "@/components/Layouts/DefaultLayout";
+// import React from "react";
+// import ChartOne from "./ChartOne";
+// import ChartTwo from "./ChartTwo";
+// import ChartThree from "./ChartThree";
+
+// const BasicChartPage: React.FC = () => {
+//   return (
+//     <DefaultLayout>
+//       <div className="grid grid-cols-12 gap-4 md:gap-6 2xl:gap-7.5">
+//         <ChartOne />
+//         <ChartTwo />
+//         <ChartThree />
+//       </div>
+//     </DefaultLayout>
+//   );
+// };
+
+// export default BasicChartPage;
+
+"use client";
+
+import dynamic from "next/dynamic";
 import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import React from "react";
-import ChartOne from "./ChartOne";
-import ChartTwo from "./ChartTwo";
-import ChartThree from "./ChartThree";
+
+const ChartOne = dynamic(() => import("./ChartOne"), { ssr: false });
+const ChartTwo = dynamic(() => import("./ChartTwo"), { ssr: false });
+const ChartThree = dynamic(() => import("./ChartThree"), { ssr: false });
 
 const BasicChartPage: React.FC = () => {
   return (
--- app-transport/src/app/page.tsx
+++ app-transport/src/app/page.tsx
@@ -44,85 +44,86 @@
 //     </AuthGuard>
 //   );
 // }
-"use client";
-import ECommerce from "@/components/Dashboard/E-commerce";
-import AuthGuard from "@/components/Auth/AuthGuard";
-import { useEffect } from "react";
-import DefaultLayout from "@/components/Layouts/DefaultLayout";
-import CrispChat from "@/components/CrispChat/CrispChat";
-import { useUserData } from "../../utils/getUserData";
-import { useRouter } from "next/navigation";
 
-export default function Home() {
-  const userData = useUserData();
+// "use client";
+// import ECommerce from "@/components/Dashboard/E-commerce";
+// import AuthGuard from "@/components/Auth/AuthGuard";
+// import { useEffect } from "react";
+// import DefaultLayout from "@/components/Layouts/DefaultLayout";
+// import CrispChat from "@/components/CrispChat/CrispChat";
+// import { useUserData } from "../../utils/getUserData";
+// import { useRouter } from "next/navigation";
 
-  console.log(userData?.isBlocked);
+// export default function Home() {
+//   const userData = useUserData();
 
-  useEffect(() => {
-    if (userData?.isBlocked && userData.isBlocked !== "false") {
-      const modal = document.getElementById(
-        "my_modal_1",
-      ) as HTMLDialogElement | null;
-      if (modal) {
-        modal.showModal();
-      }
-    }
-  }, [userData?.isBlocked]);
-  const router = useRouter();
-  function handleLogout() {
-    localStorage.removeItem("token");
-    router.push("/auth/signin");
-  }
+//   console.log(userData?.isBlocked);
 
-  // Prevent modal from closing when escape key is pressed
-  useEffect(() => {
-    const handleKeyDown = (event: KeyboardEvent) => {
-      if (event.key === "Escape") {
-        event.preventDefault();
-      }
-    };
+//   useEffect(() => {
+//     if (userData?.isBlocked && userData.isBlocked !== "false") {
+//       const modal = document.getElementById(
+//         "my_modal_1",
+//       ) as HTMLDialogElement | null;
+//       if (modal) {
+//         modal.showModal();
+//       }
+//     }
+//   }, [userData?.isBlocked]);
+//   const router = useRouter();
+//   function handleLogout() {
+//     localStorage.removeItem("token");
+//     router.push("/auth/signin");
+//   }
 
-    const modal = document.getElementById(
-      "my_modal_1",
-    ) as HTMLDialogElement | null;
-    if (modal) {
-      modal.addEventListener("keydown", handleKeyDown);
-    }
+//   // Prevent modal from closing when escape key is pressed
+//   useEffect(() => {
+//     const handleKeyDown = (event: KeyboardEvent) => {
+//       if (event.key === "Escape") {
+//         event.preventDefault();
+//       }
+//     };
 
-    return () => {
-      if (modal) {
-        modal.removeEventListener("keydown", handleKeyDown);
-      }
-    };
-  }, []);
+//     const modal = document.getElementById(
+//       "my_modal_1",
+//     ) as HTMLDialogElement | null;
+//     if (modal) {
+//       modal.addEventListener("keydown", handleKeyDown);
+//     }
 
-  return (
-    <AuthGuard redirect={"/auth/signin"}>
-      <DefaultLayout>
-        <CrispChat />
-        <dialog id="my_modal_1" className="modal">
-          <div className="modal-box">
-            <h3 className="text-lg font-bold " style={{ color: "black" }}>
-              Welcome back.
-            </h3>
-            <p className="py-4" style={{ color: "black" }}>
-              Your account has been blocked for: {userData?.isBlocked}
-            </p>
-            <div className="modal-action">
-              <form method="dialog">
-                {/* if there is a button in form, it will close the modal */}
-                <button className="btn" onClick={handleLogout}>
-                  Close
-                </button>
-              </form>
-            </div>
-          </div>
-        </dialog>
-        <ECommerce />
-      </DefaultLayout>
-    </AuthGuard>
-  );
-}
+//     return () => {
+//       if (modal) {
+//         modal.removeEventListener("keydown", handleKeyDown);
+//       }
+//     };
+//   }, []);
+
+//   return (
+//     <AuthGuard redirect={"/auth/signin"}>
+//       <DefaultLayout>
+//         <CrispChat />
+//         <dialog id="my_modal_1" className="modal">
+//           <div className="modal-box">
+//             <h3 className="text-lg font-bold " style={{ color: "black" }}>
+//               Welcome back.
+//             </h3>
+//             <p className="py-4" style={{ color: "black" }}>
+//               Your account has been blocked for: {userData?.isBlocked}
+//             </p>
+//             <div className="modal-action">
+//               <form method="dialog">
+//                 {/* if there is a button in form, it will close the modal */}
+//                 <button className="btn" onClick={handleLogout}>
+//                   Close
+//                 </button>
+//               </form>
+//             </div>
+//           </div>
+//         </dialog>
+//         <ECommerce />
+//       </DefaultLayout>
+//     </AuthGuard>
+//   );
+// }
 
 // export default function Home() {
 //   const userData = useUserData();
@@ -170,3 +171,88 @@ export default function Home() {
 //     </AuthGuard>
 //   );
 // }
+
+"use client";
+import dynamic from "next/dynamic";
+import AuthGuard from "@/components/Auth/AuthGuard";
+import { useEffect } from "react";
+import DefaultLayout from "@/components/Layouts/DefaultLayout";
+import { useUserData } from "../../utils/getUserData";
+import { useRouter } from "next/navigation";
+
+const CrispChat = dynamic(() => import("@/components/CrispChat/CrispChat"), {
+  ssr: false,
+});
+const ECommerce = dynamic(() => import("@/components/Dashboard/E-commerce"), {
+  ssr: false,
+});
+
+export default function Home() {
+  const userData = useUserData();
+  const router = useRouter();
+
+  useEffect(() => {
+    if (userData?.isBlocked && userData.isBlocked !== "false") {
+      const modal = document.getElementById(
+        "my_modal_1",
+      ) as HTMLDialogElement | null;
+      if (modal) {
+        modal.showModal();
+      }
+    }
+  }, [userData?.isBlocked]);
+
+  function handleLogout() {
+    if (typeof window !== "undefined") {
+      localStorage.removeItem("token");
+    }
+    router.push("/auth/signin");
+  }
+
+  useEffect(() => {
+    const handleKeyDown = (event: KeyboardEvent) => {
+      if (event.key === "Escape") {
+        event.preventDefault();
+      }
+    };
+
+    const modal = document.getElementById(
+      "my_modal_1",
+    ) as HTMLDialogElement | null;
+    if (modal) {
+      modal.addEventListener("keydown", handleKeyDown);
+    }
+
+    return () => {
+      if (modal) {
+        modal.removeEventListener("keydown", handleKeyDown);
+      }
+    };
+  }, []);
+
+  return (
+    <AuthGuard redirect={"/auth/signin"}>
+      <DefaultLayout>
+        <CrispChat />
+        <dialog id="my_modal_1" className="modal">
+          <div className="modal-box">
+            <h3 className="text-lg font-bold " style={{ color: "black" }}>
+              Welcome back.
+            </h3>
+            <p className="py-4" style={{ color: "black" }}>
+              Your account has been blocked for: {userData?.isBlocked}
+            </p>
+            <div className="modal-action">
+              <form method="dialog">
+                <button className="btn" onClick={handleLogout}>
+                  Close
+                </button>
+              </form>
+            </div>
+          </div>
+        </dialog>
+        <ECommerce />
+      </DefaultLayout>
+    </AuthGuard>
+  );
+}
